31700a4f1af365a069f4bf4485681415842ebcee
YOGESH S <110007804+yogesh-18@users.noreply.github.com>
2024-08-29 17:27:54 +0530

    spring-boot

---
+++ springBoot/target/classes/data.sql
@@ -0,0 +1,6 @@
+delete from person;
+INSERT INTO Person (uname, email) VALUES ('Alice', 'alice@example.com');
+INSERT INTO Person (uname, email) VALUES ('Bob', 'bob@example.com');
+INSERT INTO Person (uname, email) VALUES ('Charlie', 'charlie@example.com');
+INSERT INTO Person (uname, email) VALUES ('David', 'david@example.com');
+INSERT INTO Person (uname, email) VALUES ('Eve', 'eve@example.com');
